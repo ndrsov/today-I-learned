bb363663a5e21277d30a67427d14c65ee3011265
Andres Soria <andres.zov@gmail.com>
2023-02-15 13:25:39 -0600

    Set component declarations for the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import { Fragment } from "react";
 import "./style.css";
 
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" alt="Logo App" />
-        <h1>Today I Learned</h1>
-      </div>
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="Logo App" />
+          <h1>Today I Learned</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact Form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts List</section>;
 }
 
 export default App;
