7ebbf055fb0c36023d7f82e3a8fa31e7036234c4
Andres Soria <andres.zov@gmail.com>
2023-02-15 13:02:09 -0600

    Set up styles and logo sources

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import "./style.css";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Logo App" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
 }
 
 export default App;
