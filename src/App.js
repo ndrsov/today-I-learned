82dd094e782710e290e1fba5c61a8c2e7962be29
Andres Soria <andres.zov@gmail.com>
2023-02-23 13:07:09 -0600

    Add message when no facts for specific category exists

--- src/App.js
+++ src/App.js
@@ -168,6 +168,13 @@ function CategoryFilter({ setCurrentCategory }) {
 }
 
 function FactList({ facts }) {
+  if (facts.length === 0) {
+    return (
+      <p className="message">
+        No facts for this category yet. Create the first one!
+      </p>
+    );
+  }
   return (
     <section>
       <ul className="facts-list">
